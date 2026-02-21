819f35f3c4184ae32379782501dc8b761d11d640
Dayanna Rodriguez <l.rosd050115@itses.edu.mx>
2026-02-20 21:13:09 -0600

    Código intermedio IR, ejemplo en Java

---
+++ src/ejemploIR.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pamel
+ */
+public class ejemploIR {
+    
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int c;
+        int d;
+
+        c = a + b;
+        d = 20; //este codigo no se usa
+
+        System.out.println(c);
+    }
+    
+}
